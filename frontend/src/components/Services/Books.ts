453af989030d392fc9e465074e91237dfd22db73
Italla Felyne <italla.ferreira@riachuelo.com.br>
2024-06-09 18:30:21 -0300

    update on books

--- frontend/src/components/Services/Books.ts
+++ frontend/src/components/Services/Books.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const searchBooks = async (input: string) => {
   try {
-    const response = await axios.get(`http://localhost:3333/books`, {
+    const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/books`, {
       params: {
         search: input
       },
